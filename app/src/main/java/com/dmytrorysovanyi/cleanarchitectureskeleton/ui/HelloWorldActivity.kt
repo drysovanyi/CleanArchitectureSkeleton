bffa90d11c82d6e99421f3b46820783447921d34
Dmitry Rysovanyi <drysovanyi@lohika.com>
2023-06-03 18:29:03 +0300

    update

--- app/src/main/java/com/dmytrorysovanyi/cleanarchitectureskeleton/ui/HelloWorldActivity.kt
+++ app/src/main/java/com/dmytrorysovanyi/cleanarchitectureskeleton/ui/HelloWorldActivity.kt
@@ -1,4 +1,4 @@
-@file:OptIn(ExperimentalMaterialApi::class)
+@file:OptIn(ExperimentalMaterial3Api::class)
 
 package com.dmytrorysovanyi.cleanarchitectureskeleton.ui
 
@@ -6,21 +6,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.pullrefresh.PullRefreshIndicator
-import androidx.compose.material.pullrefresh.pullRefresh
-import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,7 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CleanArchitectureSkeletonTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting(
                         isRefreshing = viewModel.state.isRefreshing,
                         items = viewModel.state.stringList,
@@ -51,8 +47,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier, isRefreshing: Boolean, items: List<String>, onRefresh: () -> Unit) {
-    val pullRefreshState = rememberPullRefreshState(refreshing = isRefreshing, onRefresh = onRefresh)
-
     Scaffold(
         modifier = modifier,
         topBar = {
@@ -63,8 +57,7 @@ fun Greeting(modifier: Modifier = Modifier, isRefreshing: Boolean, items: List<S
             )
         },
     ) { innerPadding ->
-        Box(modifier = Modifier.pullRefresh(pullRefreshState)) {
-            PullRefreshIndicator(refreshing = isRefreshing, state = pullRefreshState)
+        Box(modifier = Modifier) {
             LazyColumn(
                 state = rememberLazyListState(),
                 contentPadding = innerPadding,
