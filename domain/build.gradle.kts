28c361abbc4f4661d3254c77ffbeebd357ec7618
Dmytro Rysovanyi <dmytror@ext.inditex.com>
2025-04-01 23:48:06 +0200

    Libraries update and improvements

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,12 +1,6 @@
 plugins {
-    id("java-library")
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ksp)
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
